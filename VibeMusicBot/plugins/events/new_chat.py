f5fdc55a950f84af7b3584904025a751f3fd0135
namandeep0045-bit <namandeep0045@gmail.com>
2026-08-12 15:31:23 +0530

    Update group join branding to Vibe

--- VibeMusicBot/plugins/events/new_chat.py
+++ VibeMusicBot/plugins/events/new_chat.py
@@ -29,7 +29,7 @@ async def new_chat_member(_, message: types.Message):
             added_by_name = added_by.mention if added_by else "ᴜɴᴋɴᴏᴡɴ"
 
             # Create the formatted message with blockquote
-            text = f"""<blockquote>🟢 <b>˹˹ʜᴀꜱɪɪ ꭙ ᴍᴜꜱɪᴄ˼ ᴀᴅᴅᴇᴅ ɪɴ ᴀ ɴᴇᴡ ɢʀᴏᴜᴘ</b></blockquote>
+            text = f"""<blockquote>🟢 <b>˹˹ᴠɪʙᴇ ꭙ ᴍᴜꜱɪᴄ˼ ᴀᴅᴅᴇᴅ ɪɴ ᴀ ɴᴇᴡ ɢʀᴏᴜᴘ</b></blockquote>
 
 <blockquote>
 🔖 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ:</b> {chat_name}
@@ -71,7 +71,7 @@ async def left_chat_member(_, message: types.Message):
         removed_by_name = removed_by.mention if removed_by else "ᴜɴᴋɴᴏᴡɴ"
 
         # Create the formatted message with blockquote
-        text = f"""<blockquote>🔴 <b>˹ʜᴀꜱɪɪ ꭙ ᴍᴜꜱɪᴄ˼ ʀᴇᴍᴏᴠᴇᴅ ꜰʀᴏᴍ ᴀ ɢʀᴏᴜᴘ</b></blockquote>
+        text = f"""<blockquote>🔴 <b>˹ᴠɪʙᴇ ꭙ ᴍᴜꜱɪᴄ˼ ʀᴇᴍᴏᴠᴇᴅ ꜰʀᴏᴍ ᴀ ɢʀᴏᴜᴘ</b></blockquote>
 
 <blockquote>
 🔖 <b>ᴄʜᴀᴛ ɴᴀᴍᴇ:</b> {chat_name}
